d07619cb048b2b9455945e1489c011fa6089504d
Pierre <pie.vancin@gmail.com>
2021-03-17 22:39:52 -0300

    feat: added pop-up

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let toggle = document.getElementById("w-open")
+let popup = document.getElementById("window")
+let close = document.getElementById("w-close")
+
+toggle.onclick = function() {
+    popup.style.display="block";
+
+};
+
+close.onclick = function() {
+    popup.style.display="none";
+}
